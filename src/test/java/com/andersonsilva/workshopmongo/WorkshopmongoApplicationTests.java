ee0f1c909b29564e2b931d7fe983371aec702f11
Anderson Silva <andssilva.23@gmail.com>
2024-06-09 15:40:54 -0400

    feat: :fire: criando projeto!

---
+++ src/test/java/com/andersonsilva/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonsilva.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
