1e85483ba4662c4eb233c12d830b605be27b6f29
Anderson Silva <andssilva.23@gmail.com>
2024-07-21 14:46:24 -0400

    feat: :fire: obtendo usuário por id

--- src/main/java/com/andersonsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/andersonsilva/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andersonsilva.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andersonsilva.workshopmongo.domain.User;
 import com.andersonsilva.workshopmongo.repository.UserRepository;
+import com.andersonsilva.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 }
